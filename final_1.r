bf6ce64827703f1962f476b20315ec5227986090
Ethan Heimlich <ejheimlich@gmail.com>
2019-04-03 16:53:15 -0400

    Fixed function to reduce errors.

--- final_1.r
+++ final_1.r
@@ -1,11 +1,18 @@
 library(MatchIt)
 library(dplyr)
-library(MatchIt)
+library(cluster)
+library(clue)
 
 compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_control, percentile) {   
-
-
-    library(dplyr)
+    
+  
+    # n_treat <- 60
+    # mu_treat <- 100
+    # sd_treat <- 10
+    # mu_control <- 90
+    # sd_control <- 10
+    # percentile <- .5
+    percentile_cutoff <- as.integer(percentile * n_treat)
     
     t_1 <- rnorm(n_treat ,mean = mu_treat, sd = sd_treat) #Higher
     t_2 <- rnorm(n_treat ,mean = mu_treat, sd = sd_treat) #Lower 
@@ -15,7 +22,7 @@ compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_contr
     t_assignment<- rep(1, n_treat)
     
     c_1 <- rnorm(n_treat ,mean = mu_control, sd = sd_control)
-    c_2 <- rnorm(n_treat,mean = mu_control, sd = sd_control)
+    c_2 <- rnorm(n_treat ,mean = mu_control, sd = sd_control)
     c_3 <- rnorm(n_treat ,mean = mu_control, sd = sd_control)
     c_4 <- rnorm(n_treat ,mean = mu_control, sd = sd_control)
     c_5 <- rnorm(n_treat ,mean = mu_control, sd = sd_control)
@@ -47,8 +54,8 @@ compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_contr
     
     
    
-    mod_match <- matchit(Assignment ~ X1 + X2 + X3 + X4 + X5,
-                         method = "nearest",discard = "none", data = data_combined)
+    mod_match <- matchit(Assignment ~ X1 + X2 + X3 + X4 + X5, method = "nearest",discard = "none", data = data_combined)
+    
     prop_pairs <- mod_match$match.matrix
     
     # Vector of the difference in proportion for each treatment - control pair.
@@ -61,9 +68,9 @@ compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_contr
     
     df_propdiff <- data.frame("Treatment" = treatment_number, "Control"= rev(prop_pairs), "Propensity_Difference" = prop_difference)
     
-    sorted_propdiff <-  df_propdiff[order(Propensity_Difference),] 
+    sorted_propdiff <-  df_propdiff[order(df_propdiff$Propensity_Difference),] 
     
-    percentile_cutoff <- percentile * n_treat
+    
     
     propensity_pairs <- sorted_propdiff[1:percentile_cutoff, 1:2]
     
@@ -90,24 +97,21 @@ compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_contr
     smd_x4 <- 100 * abs((mean(x4_treat) - mean(x4_control)) / (sqrt((var(x4_treat) + var(x4_control))/2)))
     smd_x5 <- 100 * abs((mean(x5_treat) - mean(x5_control)) / (sqrt((var(x5_treat) + var(x5_control))/2)))
     
-    propensity_asmd <- mean(smd_x1, smd_x2 ,smd_x3 , smd_x4 , smd_x5)
+    propensity_asmd <- mean(smd_x1,smd_x2, smd_x3, smd_x4, smd_x5)
     
     
     
     
     #-----------------------------------------------------Hungarian Method-------------------------------
-    library(dplyr)
-    library(cluster)
-    library(clue)
+ 
     
-    n <-nrow(data_combined)
     
     data_diss <- data_combined %>%
       select(X1, X2, X3, X4, X5)
     
     dissimilarity_matrix <- as.matrix(daisy(data_diss, stand = TRUE))
     
-    hungarian_matrix <- dissimilarity_matrix[1:(n/2),((n/2)+ 1): n]
+    hungarian_matrix <- dissimilarity_matrix[1:n_treat, (n_treat+ 1): nrow(data_combined)]
     
     
     f <-solve_LSAP(hungarian_matrix, maximum = FALSE)
@@ -155,27 +159,30 @@ compare_hungtoprop <- function(n_treat, mu_treat, sd_treat, mu_control, sd_contr
     hung_smd_x4 <- 100 * abs((mean(hung_x4_treat) - mean(hung_x4_control)) / (sqrt((var(hung_x4_treat) + var(hung_x4_control))/2)))
     hung_smd_x5 <- 100 * abs((mean(hung_x5_treat) - mean(hung_x5_control)) / (sqrt((var(hung_x5_treat) + var(hung_x5_control))/2)))
     
-    hung_asmd <- mean(hung_smd_x1, hung_smd_x2 ,hung_smd_x3 , hung_smd_x4 , hung_smd_x5)
+    hung_asmd <- mean(hung_smd_x1, hung_smd_x2 , hung_smd_x3 , hung_smd_x4 ,hung_smd_x5)
     
     
     
     #----------------------------------------Calculate original ASMD---------------------------------------------
-    orig_smd_x1 <- 100 * abs((mean(t_1) - mean(c_1)) / (sqrt((var(t_1) + var(c_1))/2)))
-    orig_smd_x2 <- 100 * abs((mean(t_2) - mean(c_2)) / (sqrt((var(t_2) + var(c_2))/2)))
-    orig_smd_x3 <- 100 * abs((mean(t_3) - mean(c_3)) / (sqrt((var(t_3) + var(c_3))/2)))
-    orig_smd_x4 <- 100 * abs((mean(t_4) - mean(c_4)) / (sqrt((var(t_4) + var(c_4))/2)))
-    orig_smd_x5 <- 100 * abs((mean(t_5) - mean(c_5)) / (sqrt((var(t_5) + var(c_5))/2)))
-    
-    orig_asmd <- mean(orig_smd_x1, orig_smd_x2, orig_smd_x3, orig_smd_x4, orig_smd_x5)
+    # orig_smd_x1 <- 100 * abs((mean(t_1) - mean(c_1)) / (sqrt((var(t_1) + var(c_1))/2)))
+    # orig_smd_x2 <- 100 * abs((mean(t_2) - mean(c_2)) / (sqrt((var(t_2) + var(c_2))/2)))
+    # orig_smd_x3 <- 100 * abs((mean(t_3) - mean(c_3)) / (sqrt((var(t_3) + var(c_3))/2)))
+    # orig_smd_x4 <- 100 * abs((mean(t_4) - mean(c_4)) / (sqrt((var(t_4) + var(c_4))/2)))
+    # orig_smd_x5 <- 100 * abs((mean(t_5) - mean(c_5)) / (sqrt((var(t_5) + var(c_5))/2)))
+    # 
+    # orig_asmd <- mean(orig_smd_x1, orig_smd_x2, orig_smd_x3, orig_smd_x4, orig_smd_x5)
     
     #--------------------------------------Compare hungarian to propensity
-    if(hung_asmd < propensity_asmd){
-      return(1)
-    } else{
-      return(0)
-    }
+    # if(isTRUE(hung_asmd < propensity_asmd)){
+    #   return(1)
+    # } else{
+    #   return(0)
+    # }
+    return(hung_asmd <= propensity_asmd)
     
 }
-
-
-compare_hungtoprop(20, 100, 10, 100, 10, .8)
+results <- c()
+for (i in 1:100){
+results <- c(compare_hungtoprop(6, 100, 10, 100, 10, .5), results)
+}
+sum(results)
